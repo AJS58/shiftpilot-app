d7e9b813c3b8368143d30f53fe56fc5d5b0b057d
AJS58 <anthony.sinfield23@gmail.com>
2026-05-18 15:23:54 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = "shiftpilot-v14-settings-darkmode";
+const FILES = ["./", "./index.html", "./manifest.json", "./sw.js", "./icon-192.png", "./icon-512.png", "./splash-screen.png"];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(key => key !== CACHE_NAME ? caches.delete(key) : null)))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request)));
+});
